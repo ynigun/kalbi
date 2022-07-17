520590a8d1043d8ec725c6e6a6b70b661c9c00dc
y_rosen <30502350+ynigun@users.noreply.github.com>
2022-07-17 17:26:09 +0300

    Update logger.go

--- log/logger.go
+++ log/logger.go
@@ -8,10 +8,10 @@ var Log = logrus.New()
 
 func init() {
 	Log.Out = os.Stdout
-	logrus.SetLevel(logrus.PanicLevel)
+	Log.Level = logrus.PanicLevel
 
 }
 
 func SetLevel(Level logrus.Level) {
-	logrus.SetLevel(Level)
+		Log.Level = logrus.PanicLevel
 }
